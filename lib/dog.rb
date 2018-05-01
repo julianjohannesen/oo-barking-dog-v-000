08fada0bd7b2825d7bdedbac908b77f0286bf041
Julian Johannesen <julian.johannesen@gmail.com>
2018-05-01 19:12:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def bark
+    puts "Bark!"
+  end
+
+end
